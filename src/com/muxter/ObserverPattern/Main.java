4be404e9481cb9aa2372fd542b1b47f19300c17f
matao <matao.mardle@gmail.com>
2017-02-07 17:03:49 +0800

    [REV] Revise gitignore

---
+++ src/com/muxter/ObserverPattern/Main.java
@@ -0,0 +1,10 @@
+package com.muxter.ObserverPattern;
+
+/**
+ * Created by matao on 07/02/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
