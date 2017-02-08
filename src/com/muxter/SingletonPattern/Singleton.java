eff11a8a42781fd24da6ffc3cee6bdcd022b22b1
matao <matao.mardle@gmail.com>
2017-02-08 15:43:48 +0800

    [ADD] Add singleton pattern

---
+++ src/com/muxter/SingletonPattern/Singleton.java
@@ -0,0 +1,21 @@
+package com.muxter.SingletonPattern;
+
+/**
+ * Created by matao on 08/02/2017.
+ */
+public class Singleton {
+    private volatile static Singleton instance;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+    }
+}
